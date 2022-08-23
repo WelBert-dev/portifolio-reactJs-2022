d715fe94d0c77e7e93e0aa9cab77ac1c8e157b52
WelBert-dev <wellison.bertelli@hotmail.com>
2022-08-22 22:47:13 -0300

    Melhora o designer da navbar e header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,22 +12,26 @@ function App() {
             <li><a href="#main-so" rel="Link S.O's">S.O</a></li>
             <li><a href="#main-linguagens" rel="Link Linguagens">Linguagens</a></li>
             <li><a href="#main-frameworks" rel="Link Frameworks">Fremeworks</a></li>
-            <li><a href="#main-formacao" rel="Link Formação">Formação Academica</a></li>
+            <li><a href="#main-formacao" rel="Link Formação">Formação</a></li>
             <li><a href="#main-interesses" rel="Link Interesses">Interesses</a></li>
             <li><a href="#main-projetos" rel="Link Projetos">Projetos</a></li>
           </ul>
         </nav>
         <div class="main-header--container">
           <div class="main-header--left">
-
+            <div>
+              <h1>Desenvolvedor</h1>
+              <h1>Web2.0</h1>
+              <h1>FullStack</h1>
+            </div>
           </div>
-          <div class="main-header--right" style={{backgroundImage: ` url(${WelbertBg})`}}>
+          <div class="main-header--right" style={{backgroundImage: `linear-gradient(to right, #b5b5b5 2%, transparent 25%), url(${WelbertBg})`}}>
 
           </div>
         </div>
       </header>
       <main>
-        <section id="main-sobre">
+        <section id="main-sobre--container">
           <div>
 
           </div>
