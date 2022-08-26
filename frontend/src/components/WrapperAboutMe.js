9f8f27d2fe9e339b7e14bae8d8502440da4786e9
WelBert-dev <wellison.bertelli@hotmail.com>
2022-08-26 18:15:37 -0300

    Componentiza a seção about me

---
+++ frontend/src/components/WrapperAboutMe.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './WrapperAboutMe.css';
+
+export default function WrapperAboutMe() {
+    return (
+        <section id="main-sobre--container">
+            <div>
+                <h1 className="main-title">&lsaquo;Sobre Mim/&rsaquo;</h1>
+                <p>
+                Desenvolvedor e entusiasta do mundo da programação e tecnologia, focado na web2.0 de ponta a ponta 
+                com boa base na stack BackEnd e atualmente focado nas tecnologias FrontEnd, buscando experiência
+                em projetos reais e/ou em produção, vivendo e estudando programação todos os dias de segunda a segunda a 2 anos,
+                Obs: Trabalhando como freelancer nos dias atuais.
+                </p>
+                <ul>
+                    <li>Nacionalidade: Brasileira</li>
+                    <li>Idade: 22 Anos</li>
+                    <li>Endereço: Rua Grécia, 544 Jardim Celani Salto - SP</li>
+                    <li>CEP: 13326-130</li>
+                </ul>
+            </div>
+        </section>
+    )
+};
